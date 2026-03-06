2303a6090bd50dc7443a36f5dee18ead9935219d
Aditya Kale <adityakale6805@gmail.com>
2026-03-07 04:52:22 +0530

    Fix CORS and cookie settings for production login

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -156,12 +156,14 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
       }
       res.cookie("token", token, options).status(200).json({
         success: true,
         token,
         user,
-        message: `User Login Success`,
+        message: "User Login Success",
       })
     } else {
       return res.status(401).json({
