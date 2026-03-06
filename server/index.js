7d6b272efd829d40af03a220cded901c9a262462
Aditya Kale <adityakale6805@gmail.com>
2026-03-07 03:48:02 +0530

    Updated CORS for deployment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		//connect frontend to backend
+		// origin:"http://localhost:3000",
+		origin:"*",
 		credentials:true,
 	})
 )
